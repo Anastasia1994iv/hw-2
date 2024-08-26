bcce20c526d9c355cc15bf34f4309eb8d0258aed
Anastasia <lovly7@yandex.ru>
2024-08-26 15:18:25 +0300

    02-02

--- src/s2-homeworks/hw02/affairs/affair/Affair.tsx
+++ src/s2-homeworks/hw02/affairs/affair/Affair.tsx
@@ -17,7 +17,7 @@ function Affair({affair, deleteAffairCallback}: AffairPropsType) {
     const nameClass = s.name + ' ' + s2[affair.priority]
     const buttonClass = s.closeButton + ' ' + s2[affair.priority]
     const affairClass = s.affair + ' ' + s2[affair.priority]
-
+   
     return (
         <div
             id={'hw2-affair-' + affair._id}
@@ -25,6 +25,7 @@ function Affair({affair, deleteAffairCallback}: AffairPropsType) {
         >
             <div id={'hw2-name-' + affair._id} className={nameClass}>
                 {/*создаёт студент*/}
+                
 {affair.name}
                 {/**/}
             </div>
