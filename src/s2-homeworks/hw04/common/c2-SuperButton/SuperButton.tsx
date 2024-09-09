38fc1794ae14ed0f39c08da027a85ef6ac164e06
Anastasia <lovly7@yandex.ru>
2024-09-09 15:35:58 +0300

    02-04

--- src/s2-homeworks/hw04/common/c2-SuperButton/SuperButton.tsx
+++ src/s2-homeworks/hw04/common/c2-SuperButton/SuperButton.tsx
@@ -1,36 +1,38 @@
-import React, {ButtonHTMLAttributes, DetailedHTMLProps} from 'react'
-import s from './SuperButton.module.css'
+import React, { ButtonHTMLAttributes, DetailedHTMLProps } from 'react';
+import s from './SuperButton.module.css';
 
-// тип пропсов обычной кнопки, children в котором храниться название кнопки там уже описан
-type DefaultButtonPropsType = DetailedHTMLProps<ButtonHTMLAttributes<HTMLButtonElement>,
-    HTMLButtonElement>
+type DefaultButtonPropsType = DetailedHTMLProps<ButtonHTMLAttributes<HTMLButtonElement>, HTMLButtonElement>;
 
 type SuperButtonPropsType = DefaultButtonPropsType & {
-    xType?: string
-}
+    xType?: string;
+};
 
-const SuperButton: React.FC<SuperButtonPropsType> = (
-    {
-        xType,
-        className,
-        disabled,
-        ...restProps // все остальные пропсы попадут в объект restProps, там же будет children
-    }
-) => {
+const SuperButton: React.FC<SuperButtonPropsType> = ({
+    xType,
+    className,
+    disabled,
+    ...restProps
+}) => {
     const finalClassName = s.button
-        // + (disabled
-        //         ? ...
-        //         : xType === 'red'
-        //             ? ...
-        + (className ? ' ' + className : '') // задачка на смешивание классов
+        + (disabled
+                ? ' ' + s.disabled
+                : xType === 'red'
+                    ? ' ' + s.red 
+                    : xType === 'secondary'
+                        ? ' ' + s.secondary
+                        : xType === 'default'
+                            ? ' ' + s.default
+                            : ''
+        )
+        + (className ? ' ' + className : '');
 
     return (
         <button
             disabled={disabled}
             className={finalClassName}
-            {...restProps} // отдаём кнопке остальные пропсы если они есть (children там внутри)
+            {...restProps}
         />
-    )
-}
+    );
+};
 
-export default SuperButton
+export default SuperButton;
