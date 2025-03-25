28ace3a2c82a5259f858d3eb6dcb2a7d851f2814
Anastasia <lovly7@yandex.ru>
2025-03-25 20:58:45 +0300

    4-3

--- src/s2-homeworks/hw11/HW11.tsx
+++ src/s2-homeworks/hw11/HW11.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import { useState } from 'react'
 import s from './HW11.module.css'
 import s2 from '../../s1-main/App.module.css'
 import { restoreState } from '../hw06/localStorage/localStorage'
@@ -16,13 +16,14 @@ function HW11() {
     const [value2, setValue2] = useState(restoreState<number>('hw11-value2', 100))
 
     const change = (event: any, value: any) => {
-        // пишет студент // если пришёл массив - сохранить значения в оба useState, иначе в первый
         if (value.length) {
             setValue1(value[0]);
             setValue2(value[1]);
-        } else {
+        }
+        else {
             setValue1(value);
         }
+        // пишет студент // если пришёл массив - сохранить значения в оба useState, иначе в первый
     }
 
     return (
